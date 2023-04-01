83f61546a74aa9a2bb55569f5cf4578ef4e82563
Mathys Clerget <mathysclerget+github@gmail.com>
2023-04-01 12:51:42 +0200

    feat: Add arg[1] as json file name
    fix: hide server tasks

--- sokoban/src/main/java/sokoban/SokobanMain.java
+++ sokoban/src/main/java/sokoban/SokobanMain.java
@@ -4,10 +4,12 @@ import com.codingame.gameengine.runner.SoloGameRunner;
 
 public class SokobanMain {
     public static void main(String[] args) {
-        SoloGameRunner gameRunner = new SoloGameRunner();
-        gameRunner.setAgent(Agent.class);
-        gameRunner.setTestCase("test21.json");
+        if (args.length == 1) {
+            SoloGameRunner gameRunner = new SoloGameRunner();
+            gameRunner.setAgent(Agent.class);
+            gameRunner.setTestCase(args[0]);
 
-        gameRunner.start();
+            gameRunner.start();
+        }
     }
 }
